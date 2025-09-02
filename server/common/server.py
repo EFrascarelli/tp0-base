6b763dbb16ae92c499d78298f6f80e1c1f2e83f8
EFrascarelli <efrascarelli@fi.uba.ar>
2025-09-01 23:45:41 -0300

    Se agrega la lectura y guardado de las apuestas del lado del server

--- server/common/server.py
+++ server/common/server.py
@@ -1,6 +1,8 @@
 import socket
 import logging
 import signal
+from common.bet_helper import get_bet, validate_bet, send_bet_confirmation
+from utils import load_bets, store_bets, has_won
 
 class Server:
     def __init__(self, port, listen_backlog):
@@ -13,13 +15,6 @@ class Server:
         signal.signal(signal.SIGTERM, self.__signal_handler)
 
     def run(self):
-        """
-        Dummy Server loop
-
-        Server that accept a new connections and establishes a
-        communication with a client. After client with communucation
-        finishes, servers starts to accept new connections again
-        """
 
         # TODO: Modify this program to handle signal to graceful shutdown
         # the server
@@ -31,19 +26,19 @@ class Server:
                 logging.warning('action: wait_for_connection | step: timeout | result: in_progress')
 
     def __handle_client_connection(self, client_sock):
-        """
-        Read message from a specific client socket and closes the socket
-
-        If a problem arises in the communication with the client, the
-        client socket will also be closed
-        """
         try:
             # TODO: Modify the receive to avoid short-reads
-            msg = client_sock.recv(1024).rstrip().decode('utf-8')
+
+
+            bet = get_bet(client_sock)
             addr = client_sock.getpeername()
-            logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
-            # TODO: Modify the send to avoid short-writes
-            client_sock.send("{}\n".format(msg).encode('utf-8'))
+            
+            logging.info(f'action: receive_message | result: success | ip: {addr[0]} | bet: {bet}')
+
+            if validate_bet(bet):
+                store_bets(bet)
+
+            send_bet_confirmation(client_sock, bet)
         except OSError as e:
             logging.error(f'action: receive_message | result: fail | error: {e}')
         finally:
